568cac3cdd658817362b4cfa28078493cf86f23c
digitnow <46672525+digitnow@users.noreply.github.com>
2025-12-22 17:33:56 +0100

    Create LesStudenter.java
    initiell commit

---
+++ oppgave1/LesStudenter.java
@@ -0,0 +1,18 @@
+// LesStudenter.java
+
+// 1. Definere en klasse for data om studenter (Student)
+class Student {
+
+}
+
+@Override
+public String toString() {
+    return "Student{" + .... + "}";
+}
+
+// 2. Bruk try-with-resources for sikker filbehandling og les inn data fra fil i en Java datastruktur
+public class LesStudenter {
+    public static void main(String[] args) {
+    ...
+    }
+}
